0923efa06a65023712238826cf5745273441a649
umang <umang@praxinfo.com>
2018-04-19 14:57:47 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  obj = {
+    id: '1',
+    name: 'darshan'
+  };
+  arr = ['abc', 'def', 'ghi'];
+  isTrue = false;
+  myName = 'darshan';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { MembersComponent } from './members/members.component';
+import { MyComponentComponent } from './my-component/my-component.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    MembersComponent,
+    MyComponentComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      FormsModule,
+      HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'member',
+        component: MembersComponent
+      },
+      {
+        path: 'product',
+        component: ProductComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
