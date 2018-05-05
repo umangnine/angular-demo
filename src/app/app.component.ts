a52ea9daf7561fc93fcab0fe4bd58d2e9c1c3df6
umang <umang@praxinfo.com>
2018-05-05 22:01:58 +0530

    video 9 template driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  items = ['Angular4', 'Reacts', 'Underscore'];
-  name = 'Darshan';
-  day = new Date(1993,10,28);
-  arr = [3, 7, 8, 5, 6, 1, 2];
-  newItem = '';
-  pushItem = function() {
-    if (this.newItem !== '') {
-      this.items.push(this.newItem);
-      this.newItem = '';
-    }
-  };
-  removeItem = function(index) {
-    this.items.splice(index, 1);
+  onSubmit = function (user) {
+      console.log(user);
   };
 }
